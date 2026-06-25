7fa205261ad2a13262f332759c13055772edf0f0
Nanromii <vuquangnam2005@gmail.com>
2026-06-25 22:06:45 +0700

    feat: config swagger document

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -8,6 +9,17 @@ async function bootstrap() {
 
   app.enableShutdownHooks();
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('SplitMate API')
+    .setDescription('SplitMate Backend API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -20,6 +32,14 @@ async function bootstrap() {
   app.enableCors();
 
   await app.listen(process.env.PORT || 3000);
+
+  console.log(
+    `🚀 Application is running on: http://localhost:${process.env.PORT}`,
+  );
+
+  console.log(
+    `📚 Swagger is running on: http://localhost:${process.env.PORT}/api/docs`,
+  );
 }
 
 bootstrap();
