3094626e277cd81bbb37fd13f1ba304865717365
Nanromii <vuquangnam2005@gmail.com>
2026-06-25 20:41:27 +0700

    feat(db): add connection postgres and redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfig } from './configs/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envValidationSchema } from './configs/env-validation.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}.local`,
+      validationSchema: envValidationSchema,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: databaseConfig
+    }),
+  ],
+  controllers: [
+    AppController
+  ],
+  providers: [
+    AppService
+  ],
 })
 export class AppModule {}
---
+++ src/configs/env-validation.config.ts
@@ -0,0 +1,18 @@
+import * as Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  NODE_ENV: Joi.string().required(),
+
+  PORT: Joi.number().required(),
+
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
+
+  REDIS_HOST: Joi.string().required(),
+  REDIS_PORT: Joi.number().required(),
+
+  JWT_SECRET: Joi.string().required(),
+});
